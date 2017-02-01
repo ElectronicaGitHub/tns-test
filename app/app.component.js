a8edd67ba18b4024153e086ebe3437e19d6e53fb
Филипп <filipp@Mac-mini-Filipp.local>
2017-02-01 19:20:39 +0300

    fix

---
+++ app/app.component.js
@@ -0,0 +1,20 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+        console.dump({
+            a: 1,
+            b: 2
+        });
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: "ns-app",
+        templateUrl: "app.component.html",
+    }),
+    __metadata("design:paramtypes", [])
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImFwcC5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLHNDQUEwQztBQU0xQyxJQUFhLFlBQVk7SUFDeEI7UUFDQyxPQUFPLENBQUMsSUFBSSxDQUFDO1lBQ1osQ0FBQyxFQUFHLENBQUM7WUFDTCxDQUFDLEVBQUcsQ0FBQztTQUNMLENBQUMsQ0FBQztJQUNKLENBQUM7SUFDRixtQkFBQztBQUFELENBQUMsQUFQRCxJQU9DO0FBUFksWUFBWTtJQUp4QixnQkFBUyxDQUFDO1FBQ1AsUUFBUSxFQUFFLFFBQVE7UUFDbEIsV0FBVyxFQUFFLG9CQUFvQjtLQUNwQyxDQUFDOztHQUNXLFlBQVksQ0FPeEI7QUFQWSxvQ0FBWSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCB9IGZyb20gXCJAYW5ndWxhci9jb3JlXCI7XG5cbkBDb21wb25lbnQoe1xuICAgIHNlbGVjdG9yOiBcIm5zLWFwcFwiLFxuICAgIHRlbXBsYXRlVXJsOiBcImFwcC5jb21wb25lbnQuaHRtbFwiLFxufSlcbmV4cG9ydCBjbGFzcyBBcHBDb21wb25lbnQgeyBcblx0Y29uc3RydWN0b3IoKSB7XG5cdFx0Y29uc29sZS5kdW1wKHtcblx0XHRcdGEgOiAxLCBcblx0XHRcdGIgOiAyXG5cdFx0fSk7XG5cdH1cbn1cbiJdfQ==
---
+++ platforms/ios/build/testapp.build/Debug-iphonesimulator/testapp.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/main.m \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/filipp/work/nativescript/test-app/platforms/ios/internal/TNSDebugging.h
